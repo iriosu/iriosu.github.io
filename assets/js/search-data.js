641e86f376878986d7d985ac01663fa1dc92bac3
iriosu <ignacio.rios.u@gmail.com>
2026-03-10 14:50:24 +0000

    Deploying to gh-pages from @ iriosu/iriosu.github.io@84daf52821b9d6352614f9af217d21519c86ef90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,8 +449,8 @@ ninja.data = [{
           section: "News",},{id: "news-thrilled-to-announce-that-our-paper-the-dating-heuristic-a-provably-strong-matching-algorithm-for-dating-platforms-has-been-accepted-at-manufacturing-amp-amp-service-operations-management-m-amp-amp-som-this-work-co-authored-with-alfredo-torrico-develops-provably-strong-algorithms-for-matching-in-online-dating-platforms-i-m-deeply-grateful-to-alfredo-for-being-such-a-great-co-author-and-to-the-editorial-team-and-reviewers-at-m-amp-amp-som-for-their-thoughtful-feedback-that-greatly-improved-the-paper",
           title: 'Thrilled to announce that our paper “The Dating Heuristic: A Provably Strong Matching...',
           description: "",
-          section: "News",},{id: "news-attending-the-2026-early-career-sustainable-operations-workshop-with-my-kids",
-          title: 'Attending the 2026 Early-Career Sustainable Operations Workshop With My Kids',
+          section: "News",},{id: "news-i-attended-the-2026-early-career-sustainable-operations-workshop-at-utd-with-my-kids",
+          title: 'I attended the 2026 Early-Career Sustainable Operations Workshop at UTD with my kids...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2026_02_28/";
